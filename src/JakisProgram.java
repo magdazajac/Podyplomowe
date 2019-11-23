079f4c683c02272b2b9ca505387913223e3227c1
Magdalena Dorota Zając <mazajacn@student.wszib.edu.pl>
2019-11-23 16:07:01 +0100

    Initial commit

---
+++ src/JakisProgram.java
@@ -0,0 +1,19 @@
+public class JakisProgram {
+    public static void main (String[] args){
+
+        float fah = 86;
+
+        System.out.println(fah + " stopni Farenhita to ...");
+        fah = fah = 32;
+        fah = fah / 9;
+        fah = fah * 5;
+        System.out.println(fah + " stopni Celsjusza\n");
+
+        float cel = 33;
+        System.out.println(cel + "stopni Cejsjusza to ...");
+        cel = cel * 9;
+        cel = cel / 5;
+        cel = cel + 32;
+        System.out.println(cel + "stopni Farenhita\n");
+    }
+}
